6d4926a009b541126bb88a6f859789edbd79954e
justin-0 <itsjustinoconnell@gmail.com>
2025-05-08 14:14:06 +0100

    trip card styled

--- components/trip-card.tsx
+++ components/trip-card.tsx
@@ -1,4 +1,10 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
+import {
+  ChipDirective,
+  ChipListComponent,
+  ChipsDirective,
+} from "@syncfusion/ej2-react-buttons";
+import { cn, getFirstWord } from "lib/utils";
 
 export default function TripCard({
   id,
@@ -9,8 +15,37 @@ export default function TripCard({
   price,
 }: TripCardProps) {
   return (
-    <Link>
+    <Link to={`/trips/${id}`} className="trip-card">
       <img src={imageUrl} alt={name} />
+      <article>
+        <h2>{name}</h2>
+        <figure>
+          <img
+            src="/assets/icons/location-mark.svg"
+            alt="location"
+            className="size-4"
+          />
+          <figcaption>{location}</figcaption>
+        </figure>
+      </article>
+      <div className="mt-5 pl-[18px] pr-3.5 pb-5">
+        <ChipListComponent id="travel-chip">
+          <ChipsDirective>
+            {tags.map((tag, index) => (
+              <ChipDirective
+                key={index}
+                text={getFirstWord(tag)}
+                cssClass={cn(
+                  index === 1
+                    ? "!bg-pink-50 !text-pink-500"
+                    : "!bg-success-50 !text-success-700"
+                )}
+              />
+            ))}
+          </ChipsDirective>
+        </ChipListComponent>
+      </div>
+      <article className="tripCard-pill">{price}</article>
     </Link>
   );
 }
