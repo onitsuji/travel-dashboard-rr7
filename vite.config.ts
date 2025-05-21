01bb0df90a20e142eb1066d7d75d3bf3fb1a0c84
onitsuji <itsjustinoconnell@gmail.com>
2025-05-21 12:03:00 +0100

    Sentry setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import { reactRouter } from "@react-router/dev/vite";
+import {
+  sentryReactRouter,
+  type SentryReactRouterBuildOptions,
+} from "@sentry/react-router";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  ssr: {
-    noExternal: [/@syncfusion/],
-  },
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "onitsuji",
+  project: "travel-dashboard-rr7",
+  // An auth token is required for uploading source maps.
+  authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
+  // ...
+};
+
+export default defineConfig((config) => {
+  return {
+    plugins: [
+      tailwindcss(),
+      reactRouter(),
+      tsconfigPaths(),
+      sentryReactRouter(sentryConfig, config),
+    ],
+    ssr: {
+      noExternal: [/@syncfusion/],
+    },
+  };
 });
